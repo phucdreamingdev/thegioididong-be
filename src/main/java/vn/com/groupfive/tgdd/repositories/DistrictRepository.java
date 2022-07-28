14185b4d3353efd5e552b1b07c699bafd3b4dd55
Minh <81819853+IkMinhTon@users.noreply.github.com>
2022-07-28 12:57:39 +0700

    fix library

--- src/main/java/vn/com/groupfive/tgdd/repositories/DistrictRepository.java
+++ src/main/java/vn/com/groupfive/tgdd/repositories/DistrictRepository.java
@@ -1,6 +1,9 @@
 package vn.com.groupfive.tgdd.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import vn.com.groupfive.tgdd.payload.entities.District;
