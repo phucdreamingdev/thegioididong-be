d29acb6b471e318837c122e5ecced73e3f3b4912
Minh <81819853+IkMinhTon@users.noreply.github.com>
2022-07-28 12:55:00 +0700

    fix library

--- src/main/java/vn/com/groupfive/tgdd/services/CustomerService.java
+++ src/main/java/vn/com/groupfive/tgdd/services/CustomerService.java
@@ -3,9 +3,12 @@ package vn.com.groupfive.tgdd.services;
 import java.util.List;
 
 import vn.com.groupfive.tgdd.payload.dto.BranchSlimResponeDTO;
+import vn.com.groupfive.tgdd.payload.dto.CartProductDTO;
 import vn.com.groupfive.tgdd.payload.dto.CategorySlimDTO;
+import vn.com.groupfive.tgdd.payload.dto.DistrictDTO;
 import vn.com.groupfive.tgdd.payload.dto.ProductListItemDTO;
 import vn.com.groupfive.tgdd.payload.dto.ProductSlimDTO;
+import vn.com.groupfive.tgdd.payload.dto.ProvinceDTO;
 import vn.com.groupfive.tgdd.payload.dto.VersionColorItemDTO;
 import vn.com.groupfive.tgdd.payload.entities.VersionColor;
 
