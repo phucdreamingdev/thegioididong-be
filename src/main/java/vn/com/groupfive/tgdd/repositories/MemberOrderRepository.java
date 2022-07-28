00a35e467d9151c8d3d68aa00369c20b45cb1bd1
Minh <81819853+IkMinhTon@users.noreply.github.com>
2022-07-29 00:53:50 +0700

    add method get member order

--- src/main/java/vn/com/groupfive/tgdd/repositories/MemberOrderRepository.java
+++ src/main/java/vn/com/groupfive/tgdd/repositories/MemberOrderRepository.java
@@ -1,11 +1,17 @@
 package vn.com.groupfive.tgdd.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import vn.com.groupfive.tgdd.payload.entities.MemberAddress;
 import vn.com.groupfive.tgdd.payload.entities.MemberOrder;
 
 @Repository
 public interface MemberOrderRepository extends JpaRepository<MemberOrder, Long>{
-
+	List<MemberOrder> getMemberOrdersByBranchId(@Param("branch_id") Long branchId);
+	
+	MemberOrder findTopByOrderByIdDesc();
 }
