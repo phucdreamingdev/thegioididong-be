6ab1cf458265f1a84a7a112345ddf153c7750e20
Minh <81819853+IkMinhTon@users.noreply.github.com>
2022-07-28 12:32:51 +0700

    new method
    addToCart, removeFromCart, editQuantityProductCart, changeVersionColorProductCartById, getAllCartProduct, getAllProvince, getAllDistrictByProvinceId

--- src/main/java/vn/com/groupfive/tgdd/services/CustomerServiceImpl.java
+++ src/main/java/vn/com/groupfive/tgdd/services/CustomerServiceImpl.java
@@ -53,6 +53,14 @@ public class CustomerServiceImpl implements CustomerService {
 	@Autowired
 	BranchMapper branchMapper;
 	
+	@Autowired
+	ProvinceRepository provinceRepo;
+	
+	@Autowired
+	DistrictRepository districtRepo;
+
+	private Cart cart = new Cart();
+	
 	@Override
 	public List<CategorySlimDTO> getAllCategoryByLevel(int level) {
 		return categoryMapper.categoriesToCategorySlimDtos(categoryRepository.getByLevel(level));
@@ -106,6 +114,121 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public List<BranchSlimResponeDTO> getBranchInStock(Long versionColorid, Long provinceId) {
 		return branchMapper.branchsToBranchSlimResponeDtos(branchStockRepository.getBranchInStock(versionColorid, provinceId));
+	}
+	
+	@Override
+	public String addToCart(Long versionColorId, int quantity, Long provinceId) {
+		VersionColor versionColor = versionColorRepository.getVersionColorById(versionColorId);
+		if (versionColor == null)
+			return "Not Found VersionColor Id";
+		List<Branch> listBranch = branchStockRepository.getBranchInStock(versionColorId, versionColorId);
+		int totalInStock = 0;
+		if (listBranch != null && listBranch.size() > 0) {
+			for (Branch branch : listBranch) {
+				Set<BranchStock> branchStocks = branch.getBranchStocks();
+				if (branchStocks != null && branchStocks.size() > 0) {
+					for (BranchStock branchStock : branchStocks) {
+						if (branchStock.getVersionColor().getId().equals(versionColorId)
+								&& branchStock.getStock() > 0) {
+							totalInStock += branchStock.getStock();
+						}
+					}
+				}
+			}
+			totalInStock -= quantity;
+			if (totalInStock > 0) {
+				ItemCartDTO product = new ItemCartDTO(versionColor.getId(), versionColor.getName(),
+						versionColor.getAvartar(), versionColor.getVersion().getProduct().getCategory().getName(),
+						versionColor.getVersion().getProduct().getManufacturer().getName(),
+						versionColor.getVersion().getName(), versionColor.getColor().getName(),
+						versionColor.getPrice() + "");
+				if (product != null) {
+					cart.addProduct(product, quantity);
+					return "Add To Cart Successful.";
+				} else
+					return "Add To Cart Failed! Not Found Product";
+			}
+		}
+		return "Not Enough Product In Stock";
+	}
+
+	@Override
+	public boolean removeFromCart(Long versionColorId) {
+		return cart.removeProduct(versionColorId);
+	}
+
+	@Override
+	public boolean editQuantityProductCart(Long versionColorId, int quantity) {
+		return cart.editQuantity(versionColorId, quantity);
+	}
+
+	@Override
+	public boolean changeVersionColorProductCartById(Long versionColorId, Long nVersionColorId) {
+		VersionColor versionColor = versionColorRepository.getVersionColorById(nVersionColorId);
+		if (versionColor == null)
+			return false;
+		ItemCartDTO product = new ItemCartDTO(versionColor.getId(), versionColor.getName(), versionColor.getAvartar(),
+				versionColor.getVersion().getProduct().getCategory().getName(),
+				versionColor.getVersion().getProduct().getManufacturer().getName(), versionColor.getVersion().getName(),
+				versionColor.getColor().getName(), versionColor.getPrice() + "");
+		return cart.changeVersionColorOfProduct(versionColorId, product);
+	}
+
+	@Override
+	public List<CartProductDTO> getAllCartProduct(Long provineId) {
+		Locale localeVN = new Locale("vi", "VN");
+		NumberFormat currencyVN = NumberFormat.getCurrencyInstance(localeVN);
+		List<CartProductDTO> list = cart.getCartProducts();
+		if (list != null && list.size() > 0) {
+			List<CartProductDTO> listViewCurrency = new ArrayList<>();
+			for (CartProductDTO cartProd : list) {
+				List<Branch> listBranch = branchStockRepository.getBranchInStock(cartProd.getProduct().getId(), provineId);
+				int totalInStock = 0;
+				if (listBranch != null && listBranch.size() > 0) {
+					for (Branch branch : listBranch) {
+						Set<BranchStock> branchStocks = branch.getBranchStocks();
+						if (branchStocks != null && branchStocks.size() > 0) {
+							for (BranchStock branchStock : branchStocks) {
+								if (branchStock.getVersionColor().getId().equals(cartProd.getProduct().getId())
+										&& branchStock.getStock() > 0) {
+									totalInStock += branchStock.getStock();
+								}
+							}
+						}
+					}
+					System.out.println(totalInStock);
+					totalInStock -= cartProd.getQuantity();
+					if(totalInStock > 0) {
+						ItemCartDTO product = new ItemCartDTO(cartProd.getProduct().getId(), cartProd.getProduct().getName(),
+								cartProd.getProduct().getImg(), cartProd.getProduct().getCategoryName(),
+								cartProd.getProduct().getBrand(), cartProd.getProduct().getVersionName(),
+								cartProd.getProduct().getColorName(),
+								currencyVN.format(Float.parseFloat(cartProd.getProduct().getPrice())));
+						CartProductDTO nProd = new CartProductDTO(cartProd.getQuantity(), product);
+						nProd.setTotalPrice(currencyVN.format(Float.parseFloat(cartProd.getTotalPrice())));
+						listViewCurrency.add(nProd);
+					}
+				}
+			}
+			return listViewCurrency;
+		}
+		return null;
+	}
+	
+	
+
+	public Cart getCart() {
+		return cart;
+	}
+
+	@Override
+	public List<ProvinceDTO> getAllProvince() {
+		return branchMapper.provincesToProvinceDtos(provinceRepo.findAll());
+	}
+
+	@Override
+	public List<DistrictDTO> getAllDistrictByProvinceId(Long provinceId) {
+		return branchMapper.districtsToDistrictDtos(districtRepo.getAllDistrictByProvinceId(provinceId));
 	}
 
 }
