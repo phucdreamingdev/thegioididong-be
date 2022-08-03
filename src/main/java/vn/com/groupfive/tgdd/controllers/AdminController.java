55f8ef8efbcc6066a3e0cde9dcda66372138709f
Minh <81819853+IkMinhTon@users.noreply.github.com>
2022-08-03 09:56:36 +0700

    add update order function
    updateMemberOrderStatusById

--- src/main/java/vn/com/groupfive/tgdd/controllers/AdminController.java
+++ src/main/java/vn/com/groupfive/tgdd/controllers/AdminController.java
@@ -3,58 +3,108 @@ package vn.com.groupfive.tgdd.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import vn.com.groupfive.tgdd.payload.entities.Member;
-import vn.com.groupfive.tgdd.payload.entities.OrderDetail;
-import vn.com.groupfive.tgdd.payload.entities.Promotion;
+import vn.com.groupfive.tgdd.exceptions.handlers.CrudException;
+import vn.com.groupfive.tgdd.payload.dto.CategorySlimDTO;
+import vn.com.groupfive.tgdd.payload.dto.MemberDTO;
+import vn.com.groupfive.tgdd.payload.dto.MemberOrderDTO;
+import vn.com.groupfive.tgdd.payload.dto.OrderDetailDTO;
+import vn.com.groupfive.tgdd.payload.dto.PromotionDTO;
+import vn.com.groupfive.tgdd.payload.dto.VersionColorItemDTO;
+import vn.com.groupfive.tgdd.payload.dto.VersionColorSlimDTO;
+import vn.com.groupfive.tgdd.payload.dto.request.CategoryRequest;
+import vn.com.groupfive.tgdd.payload.dto.request.PromotionRequest;
+import vn.com.groupfive.tgdd.payload.mapper.CategoryMapper;
+import vn.com.groupfive.tgdd.payload.mapper.PromotionMapper;
 import vn.com.groupfive.tgdd.services.AdminService;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
-	
+
 	@Autowired
 	AdminService adminService;
+
+	@Autowired
+	CategoryMapper categoryMapper;
 	
-	@GetMapping("/get-all-category")
-    public String getAllCategory() {
-        return adminService.getAllCategory();
-    }
-	
-	@GetMapping("/get-all-member")
-	public List<Member> getAllMember(){
-		return adminService.getAllMember();
+	@Autowired
+	PromotionMapper promotioMapper;
+
+	@PostMapping("/create-new-category")
+	public CategorySlimDTO createCategory(@RequestBody CategoryRequest category) throws CrudException {
+		return categoryMapper.categoryToCategorySlimDto(adminService.addCategory(category));
+	}
+
+	@PutMapping("/update-category/{id}")
+	public CategorySlimDTO updateCategory(@PathVariable Long id, @RequestBody CategoryRequest category)
+			throws CrudException {
+		return categoryMapper.categoryToCategorySlimDto(adminService.updateCategory(id, category));
 	}
 	
-	@GetMapping("/{id}")
-	public Member findMemberById(@PathVariable Long id) {
-		return adminService.findMemberById(id);	
+	@GetMapping("/get-all-category-by-level/{level}")
+	List<CategorySlimDTO> getCategoryByLevel(@PathVariable("level") int level) {
+		return adminService.getAllCategoryByLevel(level);
+	}
+
+	@PostMapping("/create-new-promotion")
+	public PromotionDTO createPromotion(
+			@RequestBody PromotionRequest promotion) throws CrudException {
+		return promotioMapper.promotionToPromotionDto(adminService.addPromotions(promotion));
 	}
 	
-	@GetMapping("/get-order-list")
-	public List<OrderDetail> getAllOrder(){
-		return adminService.getAllOrder();
+	@PutMapping("/update-promotion/{id}")
+	public PromotionDTO updatePromotion(@PathVariable Long id,
+			@RequestBody PromotionRequest promotion) throws CrudException {
+		return promotioMapper.promotionToPromotionDto(adminService.updatePromotions(id,promotion));
 	}
 	
-	@GetMapping("/{id}")
-	public Promotion findPromotionById(@PathVariable Long id) {
-		return adminService.findPromotionById(id);
+	@GetMapping("/get-version-color-by-id")
+	public VersionColorItemDTO getVersionColorById(Long id) {
+		return adminService.getVersionColorById(id);
 	}
 	
-	@PutMapping("/{id}")
-	public Promotion updatePromotion(@PathVariable Long id, @RequestBody Promotion promotion) {
-		return adminService.updatePromotion(id, promotion);
+	@GetMapping("/get-all-order")
+	public List<MemberOrderDTO> getAllOrders() {
+		return adminService.getAllMemberOrders();
 	}
 	
-	@DeleteMapping("/{id}")
-	public void deletePromotion(@PathVariable Long id) {
-		adminService.deletePromotion(id);
+	
+	@GetMapping("/get-all-order-by-branchid")
+	public List<MemberOrderDTO> getAllOrdersByBranchId(Long branchId) {
+		return adminService.getAllMemberOrdersByBranch(branchId);
+	}
+	
+	@GetMapping("/get-all-orderdetail-by-memberorderid")
+	public List<OrderDetailDTO> getAllOrderDetailByMemberOrderId(Long memberOrderId) {
+		return adminService.getAllOrderDetailsByMemberOrderId(memberOrderId);
+	}
+	
+	@GetMapping("/search-order-by-phone")
+	public List<MemberOrderDTO> searchOrdersByPhone(String phoneNumber) {
+		return adminService.getAllMemberOrdersByPhoneNumber(phoneNumber);
+	}
+	
+	@GetMapping("/get-member-by-id")
+	public MemberDTO findMemberById(@RequestParam(name = "id") Long id) {
+		return adminService.findMemberById(id);
+	}
+	
+	@GetMapping("/get-version-color-by-name")
+	public List<VersionColorSlimDTO> findByName(@RequestParam String name){
+		return adminService.searchVersionColorByName(name);
+	}
+	
+	@GetMapping("/update-order-by-id")
+	public boolean updateMemberOrderStatusById(Long memberOrderId, String status) {
+		return adminService.updateOrderStatusByMemberOrderId(memberOrderId, status);
 	}
 }
